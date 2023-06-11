fb2b572e5da9c79835d96329f49c8ffdb85df47b
coder <soumyodipsutra@gmail.com>
2023-06-11 23:53:27 +0530

    retrofit and data classes

--- app/src/main/java/com/example/recipeapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragments/HomeFragment.kt
@@ -1,13 +1,51 @@
 package com.example.recipeapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.R
+import com.example.recipeapp.pojo.Meal
+import com.example.recipeapp.pojo.MealList
+import com.example.recipeapp.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 
 class HomeFragment : Fragment() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList>{
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if(response.body()!=null){
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Log.d("TEST","meal id${randomMeal.idMeal} name${randomMeal.strMeal}" )
+                } else {
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("HomeFragment",t.message.toString())
+            }
+        })
+    }
 }
