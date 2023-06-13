697d4f55c624fa652643556d93a09b0a6d60de31
coder <soumyodipsutra@gmail.com>
2023-06-14 00:12:38 +0530

    some errors got

--- app/src/main/java/com/example/recipeapp/activity/MealActivity.kt
+++ app/src/main/java/com/example/recipeapp/activity/MealActivity.kt
@@ -2,11 +2,38 @@ package com.example.recipeapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.ActivityMainBinding
+import com.example.recipeapp.fragments.HomeFragment
 
 class MealActivity : AppCompatActivity() {
+    private lateinit var mealId:String
+    private lateinit var mealName:String
+    private lateinit var mealThumb:String
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_meal)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getMealInformationFromIntent()
+        setInformationInViews()
+    }
+
+    private fun setInformationInViews() {
+        Glide.with(applicationContext)
+            .load(mealThumb)
+            .into(binding.imgMealDetail)
+
+        binding.collapsingToolbar.title = mealName
+    }
+
+    private fun getMealInformationFromIntent() {
+        val intent = intent
+        mealId = intent.getStringExtra(HomeFragment.MEAL_ID)!!
+        mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
+        mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
     }
 }
