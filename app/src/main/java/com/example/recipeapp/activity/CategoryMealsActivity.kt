1fe29000b1f45127668b58db105c1d16d144f9d8
coder <soumyodipsutra@gmail.com>
2023-06-20 20:35:05 +0530

    category click

---
+++ app/src/main/java/com/example/recipeapp/activity/CategoryMealsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipeapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipeapp.R
+
+class CategoryMealsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_meals)
+    }
+}
