cabd745e672a44edced95714af77e3c4c5819f55
coder <soumyodipsutra@gmail.com>
2023-06-20 22:34:04 +0530

    items of meals of various items

--- app/src/main/java/com/example/recipeapp/activity/CategoryMealsActivity.kt
+++ app/src/main/java/com/example/recipeapp/activity/CategoryMealsActivity.kt
@@ -1,12 +1,29 @@
 package com.example.recipeapp.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.ActivityCategoryMealsBinding
+import com.example.recipeapp.fragments.HomeFragment
+import com.example.recipeapp.viewModel.CategoryMealsViewModel
 
 class CategoryMealsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCategoryMealsBinding
+    lateinit var categoryMealsViewModel: CategoryMealsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category_meals)
+        binding = ActivityCategoryMealsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        categoryMealsViewModel = ViewModelProviders.of(this)[CategoryMealsViewModel::class.java]
+
+        categoryMealsViewModel.getMealsByCategories(intent.getStringExtra(HomeFragment.CATEGORY_NAME)!!)
+
+        categoryMealsViewModel.observeMealsLiveData().observe(this, Observer { mealsList ->
+
+        })
     }
 }
--- app/src/main/java/com/example/recipeapp/retrofit/MealApi.kt
+++ app/src/main/java/com/example/recipeapp/retrofit/MealApi.kt
@@ -3,6 +3,7 @@ package com.example.recipeapp.retrofit
 import com.example.recipeapp.pojo.CategoryList
 import com.example.recipeapp.pojo.MealsByCategoryList
 import com.example.recipeapp.pojo.MealList
+import com.example.recipeapp.pojo.MealsBycategory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.Call
@@ -14,7 +15,8 @@ interface MealApi {
     fun getMealDetails(@Query("i") id:String) :Call<MealList>
     @GET("filter.php?")
     fun getPopularItems(@Query("c") categoryName:String) :Call<MealsByCategoryList>
-
     @GET("categories.php")
     fun getCategories() :Call<CategoryList>
+    @GET("filter.php")
+    fun getMealsByCategory(@Query("c") categoryName: String) :Call<MealsByCategoryList>
 }
