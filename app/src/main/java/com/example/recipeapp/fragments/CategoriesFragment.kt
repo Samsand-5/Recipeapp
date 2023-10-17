1d348cd11a59ce9b5289abee341f88868196956a
coder <soumyodipsutra@gmail.com>
2023-10-17 18:07:35 +0530

    some errors resolved

--- app/src/main/java/com/example/recipeapp/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragments/CategoriesFragment.kt
@@ -7,26 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.recipeapp.activity.MainActivity
 import com.example.recipeapp.adapters.CategoriesAdapter
 import com.example.recipeapp.databinding.FragmentCategoriesBinding
 import com.example.recipeapp.viewModel.HomeViewModel
 
 
 class CategoriesFragment : Fragment() {
-
-    private lateinit var binding: FragmentCategoriesBinding
-    private lateinit var categoriesAdapter: CategoriesAdapter
-    private lateinit var viewModel: HomeViewModel
+    lateinit var binding: FragmentCategoriesBinding
+    lateinit var categoriesAdapter: CategoriesAdapter
+    lateinit var viewModel: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentCategoriesBinding.inflate(inflater)
         return binding.root
     }
@@ -39,16 +39,17 @@ class CategoriesFragment : Fragment() {
     }
 
     private fun observeCategories() {
-        viewModel.observeCategoriesLiveData().observe(viewLifecycleOwner, Observer { categories ->
+        viewModel.observeCategoriesLiveData().observe(viewLifecycleOwner) {categories ->
             categoriesAdapter.setCategoryList(categories)
-        })
+        }
     }
 
     private fun prepareRecyclerView() {
         categoriesAdapter = CategoriesAdapter()
         binding.rvCategories.apply {
-            layoutManager = GridLayoutManager(context,3,GridLayoutManager.VERTICAL,false)
+            layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
             adapter = categoriesAdapter
         }
     }
+
 }
